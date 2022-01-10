abdd53bf148346b20d8264a266d308f8ec0b61f0
Nicolai Ommer <nicolai.ommer@qaware.de>
2022-01-10 17:49:16 +0100

    Add endpoints for default MultipartFile and non-multipart request

--- src/main/java/de/qaware/multipart/UploadController.java
+++ src/main/java/de/qaware/multipart/UploadController.java
@@ -2,16 +2,18 @@ package de.qaware.multipart;
 
 import org.apache.tomcat.util.http.fileupload.MultipartStream;
 import org.apache.tomcat.util.http.fileupload.ParameterParser;
+import org.apache.tomcat.util.http.fileupload.util.Streams;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -22,9 +24,9 @@ import java.util.zip.CheckedOutputStream;
 import java.util.zip.Checksum;
 
 @RestController
-@RequestMapping("/api/multipart")
+@RequestMapping("/api")
 public class UploadController {
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "multipart", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<List<UploadResult>> storeDocument(MultipartHttpServletRequest request) throws IOException {
         MultipartStream multipartStream = createMultipartStream(request);
         boolean hasData = multipartStream.skipPreamble();
@@ -36,19 +38,32 @@ public class UploadController {
         do {
             multipartStream.readHeaders();
             Checksum checksum = new CRC32();
-            long numBytes = readBodyData(multipartStream, checksum);
+            long numBytes = readBodyData(multipartStream.newInputStream(), checksum);
             uploadResults.add(new UploadResult(numBytes, checksum.getValue()));
         } while (multipartStream.readBoundary());
         return ResponseEntity.ok(uploadResults);
     }
 
-    private long readBodyData(MultipartStream multipartStream, Checksum checksum) throws IOException {
-        long numBytes;
+    @PostMapping(value = "multipart/file", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<UploadResult>> storeDocument(@RequestPart(name = "file1") MultipartFile multipartFile) throws IOException {
+
+        Checksum checksum = new CRC32();
+        long numBytes = readBodyData(multipartFile.getInputStream(), checksum);
+        return ResponseEntity.ok(List.of(new UploadResult(numBytes, checksum.getValue())));
+    }
+
+    @PostMapping(value = "singlepart", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<UploadResult> upload(@RequestBody InputStreamResource inputStream) throws IOException {
+        Checksum checksum = new CRC32();
+        long numBytes = readBodyData(inputStream.getInputStream(), checksum);
+        return ResponseEntity.ok(new UploadResult(numBytes, checksum.getValue()));
+    }
+
+    private long readBodyData(InputStream inputStream, Checksum checksum) throws IOException {
         try (OutputStream outputStream = OutputStream.nullOutputStream();
-                CheckedOutputStream checkedOutputStream = new CheckedOutputStream(outputStream, checksum)) {
-            numBytes = multipartStream.readBodyData(checkedOutputStream);
+             CheckedOutputStream checkedOutputStream = new CheckedOutputStream(outputStream, checksum)) {
+            return Streams.copy(inputStream, checkedOutputStream, false);
         }
-        return numBytes;
     }
 
 
